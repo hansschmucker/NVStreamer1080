f833f0580c654f88bab84f60922c2520366f9deb
hansschmucker <hansschmucker@gmail.com>
2022-07-19 23:55:42 +0200

    Initial Import

---
+++ MainUI.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace NVStreamer1080
+{
+    partial class NVStreamerMainUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.CheckTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.HideTimer = new System.Windows.Forms.Timer(this.components);
+            this.useSecondScreenCB = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // CheckTimer
+            // 
+            this.CheckTimer.Interval = 500;
+            this.CheckTimer.Tick += new System.EventHandler(this.CheckTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NVStreamer not active";
+            // 
+            // HideTimer
+            // 
+            this.HideTimer.Enabled = true;
+            this.HideTimer.Interval = 3000;
+            this.HideTimer.Tick += new System.EventHandler(this.HideTimer_Tick);
+            // 
+            // checkBox1
+            // 
+            this.useSecondScreenCB.AutoSize = true;
+            this.useSecondScreenCB.Location = new System.Drawing.Point(12, 25);
+            this.useSecondScreenCB.Name = "checkBox1";
+            this.useSecondScreenCB.Size = new System.Drawing.Size(187, 17);
+            this.useSecondScreenCB.TabIndex = 1;
+            this.useSecondScreenCB.Text = "Switch to 1080p screen if possible";
+            this.useSecondScreenCB.UseVisualStyleBackColor = true;
+            this.useSecondScreenCB.CheckedChanged += new System.EventHandler(this.OnSecondScreenCheckboxChange);
+            // 
+            // NVStreamerMainUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 61);
+            this.Controls.Add(this.useSecondScreenCB);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "NVStreamerMainUI";
+            this.Text = "NVStreamer Res Utility";
+            this.Load += new System.EventHandler(this.NVStreamerMainUI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer CheckTimer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer HideTimer;
+        private System.Windows.Forms.CheckBox useSecondScreenCB;
+    }
+}
+
---
+++ MainUI.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace NVStreamer1080
+{
+    public partial class NVStreamerMainUI : Form
+    {
+        public NVStreamerMainUI()
+        {
+            InitializeComponent();
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct DispSet
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 106)]
+            byte[] padding0;
+            public int width, height, dummy, frequency;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 32)]
+            byte[] padding1;
+        };
+
+
+
+        [DllImport("user32.dll")]
+        public static extern int EnumDisplaySettings(string a, int b, ref DispSet c);
+        [DllImport("user32.dll")]
+        public static extern int ChangeDisplaySettings(ref DispSet a, int b);
+
+
+        private int initialWidth;
+        private int initialHeight;
+        private int initialRefresh;
+        private bool useSecondScreen = false;
+        NotifyIcon trayNotifyIcon;
+        private void NVStreamerMainUI_Load(object sender, EventArgs e)
+        {
+            Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true).SetValue("NVStreamer1080", Application.ExecutablePath);
+
+            var disp = new DispSet();
+            if (EnumDisplaySettings(null, -1, ref disp) == 0)
+            {
+                Application.Exit();
+                return;
+            }
+
+            initialWidth = disp.width;
+            initialHeight = disp.height;
+            initialRefresh = disp.frequency;
+
+            
+            var trayContextMenu = new ContextMenu();
+            trayNotifyIcon = new NotifyIcon();
+            IntPtr tryIconPtr = Resource1.TrayIcon.Handle;
+            Icon trayIcon = Icon.FromHandle(tryIconPtr);
+
+            trayContextMenu.MenuItems.Add("Show UI", OnShow);
+            trayContextMenu.MenuItems.Add("Exit", OnExit);
+            if (Registry.CurrentUser.OpenSubKey("SOFTWARE\\TapperWare\\NVStreamer1080", true) == null)
+                Registry.CurrentUser.CreateSubKey("SOFTWARE\\TapperWare\\NVStreamer1080", true);
+            useSecondScreen=((string)Registry.CurrentUser.OpenSubKey("SOFTWARE\\TapperWare\\NVStreamer1080", true).GetValue("UseSecondScreen","0"))=="1";
+            if (useSecondScreen)
+                useSecondScreenCB.Checked = true;
+
+            trayNotifyIcon.Icon = trayIcon;
+            trayNotifyIcon.Text = "NV Streamer 1080";
+            trayNotifyIcon.Click += OnShow;
+
+            trayNotifyIcon.ContextMenu = trayContextMenu;
+            trayNotifyIcon.Visible = true;
+            CheckTimer.Enabled = true;
+
+        }
+
+        private bool allowClose = false;
+
+        protected override void Dispose(bool isDisposing)
+        {
+            if (!allowClose)
+            {
+                ShowInTaskbar = false;
+                Visible = false;
+                return;
+            }
+
+            if ((components != null))
+            {
+                components.Dispose();
+            }
+
+            if (isDisposing)
+            {
+                trayNotifyIcon.Dispose();
+            }
+
+            if (nv1080Set)
+            {
+                SetResolution(initialWidth, initialHeight, initialRefresh);
+                nv1080Set = false;
+            }
+
+            base.Dispose(isDisposing);
+
+        }
+
+            private void OnExit(object sender, EventArgs e)
+        {
+            allowClose = true;
+            Application.Exit();
+        }
+
+        private void OnShow(object sender, EventArgs e)
+        {
+            ShowInTaskbar = true;
+            Visible = true;
+            BringToFront();
+            CenterToScreen();
+        }
+
+        private bool nv1080Set = false;
+
+        private void SetResolution(int w,int h, int r)
+        {
+
+            var disp = new DispSet();
+            if (EnumDisplaySettings(null, -1, ref disp) == 0)
+                return;
+
+            disp.width = w;
+            disp.height = h;
+            disp.frequency = r;
+            ChangeDisplaySettings(ref disp, 1);
+
+        }
+
+        private void CheckTimer_Tick(object sender, EventArgs e)
+        {
+            var Switch = Path.Combine(Environment.SystemDirectory, "DisplaySwitch.exe");
+            var nvRunning = Process.GetProcesses().Where(a => a.ProcessName.ToLower() == "nvstreamer").Count() > 0;
+            if(nvRunning && !nv1080Set)
+            {
+                if (useSecondScreen)
+                {
+                    
+                    Process.Start(Switch, "/external");
+                    label1.Text = "NVStreamer active: External";
+                }
+                else
+                {
+                    SetResolution(1920, 1080, 60);
+                    label1.Text = "NVStreamer active: 1920x1080@60";
+                }
+                nv1080Set = true;
+                useSecondScreenCB.Enabled = false;
+            }
+            else if(!nvRunning && nv1080Set)
+            {
+                if (useSecondScreen)
+                {
+                    Process.Start(Switch, "/internal");
+                }
+                else
+                {
+                    SetResolution(initialWidth, initialHeight, initialRefresh);
+                }
+                nv1080Set = false;
+                label1.Text = "NVStreamer not active";
+                useSecondScreenCB.Enabled = true;
+            }
+        }
+
+        private void HideTimer_Tick(object sender, EventArgs e)
+        {
+            ShowInTaskbar = false;
+            Visible = false;
+            HideTimer.Enabled = false;
+
+        }
+
+        private void OnSecondScreenCheckboxChange(object sender, EventArgs e)
+        {
+            Registry.CurrentUser.OpenSubKey("SOFTWARE\\TapperWare\\NVStreamer1080", true).SetValue("UseSecondScreen", useSecondScreenCB.Checked ? "1" : "0", RegistryValueKind.String);
+            useSecondScreen = useSecondScreenCB.Checked;
+
+        }
+       
+    }
+}
